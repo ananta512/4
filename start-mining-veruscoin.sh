5a62ff930eb8ba33e5fa829cf4b3a339524c120d
ananta512 <ananta_kansakar@live.com>
2021-11-24 21:41:51 +0545

    Add files via upload

---
+++ start-mining-veruscoin.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#This is an example you can edit and use
+#There are numerous parameters you can set, please check Help and Examples folder
+
+./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool verushash.eu.mine.zergpool.com:3300 -wallet LaxRo693Wz4f4szXKavoRLuft5YCnfyorf -p c=LTC,mc=VRSC,ID=pc1
